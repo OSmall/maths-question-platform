0664ff18be1cfbdbebf64e7d15267cb19a6ceadb
OSmall <oliver.small232@gmail.com>
2026-03-01 22:15:48 +1100

    #6 use stable names in errors

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,30 +1,33 @@
 class AppError extends Error {
+  static ERROR_NAME = 'APP_ERROR'
   constructor(message: string, options: ErrorOptions = {}) {
     super(message, options)
-    this.name = new.target.name
+    const ctor = this.constructor as typeof AppError // reads the static value even if it is overwritten by child classes
+    this.name = ctor.ERROR_NAME
     Object.setPrototypeOf(this, new.target.prototype)
   }
 }
 
 export class NotFoundError extends AppError {
+  static ERROR_NAME = 'NOT_FOUND_ERROR'
   constructor(entity: string, identifier: string | number, options: ErrorOptions = {}) {
     super(`Entity ${entity} identified by ${identifier} was not found`, options)
   }
 }
 
 export class PayloadQueryError extends AppError {
+  static ERROR_NAME = 'PAYLOAD_QUERY_ERROR'
   constructor(cause: unknown) {
     super(`Error when querying PayloadCMS`, { cause })
   }
 }
 
 export class FormatError extends AppError {
-  constructor(message: string, options: ErrorOptions = {}) {
-    super(message, options)
-  }
+  static ERROR_NAME = 'FORMAT_ERROR'
 }
 
 export class NotANumberError extends FormatError {
+  static ERROR_NAME = 'NOT_A_NUMBER_ERROR'
   constructor(value: unknown, options: ErrorOptions = {}) {
     super(`${value} is not a number`, options)
   }
